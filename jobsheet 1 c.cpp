ef72bb2b9281ac9caea25f8d12935087cfd5dce6
taufikhidayat135 <64574537+taufikhidayat135@users.noreply.github.com>
2020-05-11 14:14:44 +0700

    Create jobsheet 1 c.cpp
    Buatlah program array satu dimensi yang menampilkan NIM masing asing mahasiswa

---
+++ jobsheet 1 c.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int array[5] = {5,7,10,7,6};
+printf("Data Ke-1 = %d\n",array[0]);
+printf("Data Ke-2 = %d\n",array[1]);
+printf("Data Ke-3 = %d\n",array[2]);
+printf("Data Ke-4 = %d\n",array[3]);
+printf("Data Ke-5 = %d\n",array[4]);
+	
+}
