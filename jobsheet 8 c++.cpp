c615483871dac2b6e0648d00de761e5df38aff7b
taufikhidayat135 <64574537+taufikhidayat135@users.noreply.github.com>
2020-05-11 14:34:03 +0700

    Create jobsheet 8 c++.cpp
    Mengisi dan menampilkan elemen array menggunakan pengulangan

---
+++ jobsheet 8 c++.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+int nim[5],i;
+for(i-0;i<=4;i++){
+cout<<"Masukkan Nilai "<<i+1<<" : ";
+cin>>nim[i];
+cout<<endl;
+for(i=0;i<=4;i++){
+cout<<"Nilai ke"<<i+1<<"="<<nim[i]<<endl;
+}
+cout<<endl;
+}
+}
