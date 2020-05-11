6959c04e0a361e7c9273bcbfff6c81783b8e2fc4
taufikhidayat135 <64574537+taufikhidayat135@users.noreply.github.com>
2020-05-11 14:21:41 +0700

    Create jobsheet 4 c.cpp
    Buatlah program pengurangan matriks dua dimensi dengan menggunakan Nim dua orang mahasiswa

---
+++ jobsheet 4 c.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <conio.h>
+Int main (){
+int matrika [2][2];
+int matrikb [2][2];
+int hasil [2][2];
+int i, j;
+printf ("Pengurangan Matriks\n");
+for (i = 0; i< 2; i++){
+for (j = 0; j < 2; j++){
+printf ("Masukkan nilai index %d, %d = ", i, j);
+scanf ("%d", &matrika[i][j]);
+}
+}
+printf ("\n");
+for (i = 0; i< 2; i++){
+for (j = 0; j < 2; j++){
+printf ("Masukkan nilai index %d, %d = ", i, j);
+scanf ("%d", &matrikb[i][j]);
+}
+}for (i = 0; i< 2; i++){
+for (j = 0; j < 2; j++){
+hasil [i][j]=matrika[i][j]-matrikb[i][j];
+printf ("%d ", hasil[i][j]);
+}
+printf ("\n");
+}
+getch();
+}
